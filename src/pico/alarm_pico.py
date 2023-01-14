fa218ec7ce568eabf1b081112dd52c8b2932293e
Christian Flinker <info@sandbeck.net>
2023-01-14 23:38:41 +0100

    pico version i foerste udgave

--- src/pico/alarm_pico.py
+++ src/pico/alarm_pico.py
@@ -1,14 +1,70 @@
-import machine
-import utime
+from machine import Pin
+from utime import sleep_ms
 
 PIN_PIR = 28
+PIN_LYS = 25
+PIN_KNAP = 15
+PIN_SIRENE = 16
 
-pir = machine.Pin(PIN_PIR, machine.Pin.IN, machine.Pin.PULL_DOWN)
+pir = Pin(PIN_PIR, Pin.IN, Pin.PULL_DOWN)
+knap = Pin(PIN_KNAP, Pin.IN, Pin.PULL_DOWN)
+lys = Pin(PIN_LYS, Pin.OUT)
+sirene = Pin(PIN_SIRENE, Pin.OUT)
 
-def pir_handler(pin):
-    utime.sleep_ms(100)
+alarmAktiv = False
+
+# Interrupt haandtering
+def pir_handler(pin: Pin):
+    sleep_ms(100)
+    if pin.value() and alarmAktiv:
+        print("ALARM! Du er opdaget!")
+        lys.on()
+        sirene.on()
+
+def slaa_alarm_til(pin: Pin):
+    global alarmAktiv
+    sleep_ms(2000)
     if pin.value():
-        print("ALARM! Bevaegelse detekteret")
+        print("Slaar alarm til om 10 sekunder")
+        # nedtaelling med lys og lyd
+        nedtaelling(9)
+        # sluk sirene og lys
+        lys.off()
+        sirene.off()
+        # aktiver alarm
+        alarmAktiv = True
+        
+        # knap skal nu deaktivere alarm
+        knap.irq(trigger=Pin.IRQ_RISING, handler=slaa_alarm_fra)
 
-pir.irq(trigger=machine.Pin.IRQ_RISING, handler=pir_handler)
+def slaa_alarm_fra(pin: Pin):
+    global alarmAktiv
+    sleep_ms(2000)
+    if pin.value():
+        print("Alarm deaktiveret")
+        alarmAktiv = False
+        lys.off()
+        sirene.off()
+        knap.irq(trigger=Pin.IRQ_RISING, handler=slaa_alarm_til)
 
+# hjaelpemetoder
+def nedtaelling(fra: int):
+    for i in range(fra):
+        sleep_ms(900)
+        print( f'{fra - i} {"..."}')
+        lys.on()
+        sleep_ms(100)
+        lys.off()
+    lys.on()
+    print("Slaaet til")
+    sleep_ms(1000)
+
+# Opstart
+print("Starter alarm...")
+lys.value(0)
+sirene.value(0)
+knap.irq(trigger=Pin.IRQ_RISING, handler=slaa_alarm_til)
+# aktiver pir
+pir.irq(trigger=Pin.IRQ_RISING, handler=pir_handler)
+
+print("Klar!")
